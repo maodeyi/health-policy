4cf9d167ca8a9e42e4c892cc791298ecf018537c
maodeyi <505287796@qq.com>
2018-07-13 16:12:54 +0800

    update

--- policy.py
+++ policy.py
@@ -23,14 +23,17 @@ def cluster_status():
 	policy = load_policy()
 	print(policy['console'])
 
+	print(len(policy['policy']))
 	health = True
 	for index_policy in range(len(policy['policy'])):	
-		if health == False:
-			break
 		response = get_pods_status(policy['console']['host'], policy['console']['port'], policy['policy'][index_policy]['namespace']['name'])
 
 		if response.status == 200 :
+			print(200)
 			data = json.loads(response.read())
+			print(data)
+			if data == "":
+				return 'unhealth'				
 			for index_policy_pod in range(len(policy['policy'][index_policy]['namespace']['pod'])):
 				for index in range(len(data['pods'])):
 					if policy['policy'][index_policy]['namespace']['pod'][index_policy_pod]['running_num'] <= 0:
@@ -39,12 +42,10 @@ def cluster_status():
 						policy['policy'][index_policy]['namespace']['pod'][index_policy_pod]['running_num']-=1
 			
 				if policy['policy'][index_policy]['namespace']['pod'][index_policy_pod]['running_num'] > 0:
-					health = False
-					break
- 		
-	
-	if health == False:
-		return 'unhealth'
+ 	
+					return 'unhealth'	
+		else :
+			return 'console unhealth'	
 
 	return 'health' 
 
