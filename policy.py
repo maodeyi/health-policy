e627ef1c223778b450a4029beafd85c55d81969b
maodeyi <505287796@qq.com>
2018-07-16 11:34:15 +0800

    try

--- policy.py
+++ policy.py
@@ -9,45 +9,53 @@ def load_policy():
 	return data
 
 def get_pods_status(host, port, namespace):
-	print(host)
-	print(port)
-	print(namespace)
-	conn = http.client.HTTPConnection(host, port)
-	conn.request("GET", "/kubernetes/pods?namespace=" + namespace)
-	response = conn.getresponse()
-	return response
+	try:
+		print(host)
+		print(port)
+		print(namespace)
+		conn = http.client.HTTPConnection(host, port)
+		conn.request("GET", "/kubernetes/pods?namespace=" + namespace)
+		response = conn.getresponse()
+		return response
+	except Exception:
+		return ""
 	
 
 @app.route('/cluster_status')
 def cluster_status():
-	policy = load_policy()
-	print(policy['console'])
+	try:
+		policy = load_policy()
+		print(policy['console'])
 
-	print(len(policy['policy']))
-	health = True
-	for index_policy in range(len(policy['policy'])):	
-		response = get_pods_status(policy['console']['host'], policy['console']['port'], policy['policy'][index_policy]['namespace']['name'])
-
-		if response.status == 200 :
-			print(200)
-			data = json.loads(response.read())
-			print(data)
-			if data == "":
-				return 'unhealth'				
-			for index_policy_pod in range(len(policy['policy'][index_policy]['namespace']['pod'])):
-				for index in range(len(data['pods'])):
-					if policy['policy'][index_policy]['namespace']['pod'][index_policy_pod]['running_num'] <= 0:
-						break
-					if data['pods'][index]['name'].startswith(policy['policy'][index_policy]['namespace']['pod'][index_policy_pod]['name']):
-						policy['policy'][index_policy]['namespace']['pod'][index_policy_pod]['running_num']-=1
+		print(len(policy['policy']))
+		health = True
+		for index_policy in range(len(policy['policy'])):	
+			response = get_pods_status(policy['console']['host'], policy['console']['port'], policy['policy'][index_policy]['namespace']['name'])
+			if response == "" :
+				return "excetipn"
+			if response.status == 200 :
+				print(200)
+				data = json.loads(response.read())
+				print(data)
+				if data == "":
+					return 'unhealth'				
+				for index_policy_pod in range(len(policy['policy'][index_policy]['namespace']['pod'])):
+					for index in range(len(data['pods'])):
+						if policy['policy'][index_policy]['namespace']['pod'][index_policy_pod]['running_num'] <= 0:
+							break
+						if data['pods'][index]['name'].startswith(policy['policy'][index_policy]['namespace']['pod'][index_policy_pod]['name']):
+							if data['pods'][index]['status'] == "Running" :
+								policy['policy'][index_policy]['namespace']['pod'][index_policy_pod]['running_num']-=1
 			
-				if policy['policy'][index_policy]['namespace']['pod'][index_policy_pod]['running_num'] > 0:
+					if policy['policy'][index_policy]['namespace']['pod'][index_policy_pod]['running_num'] > 0:
  	
-					return 'unhealth'	
-		else :
-			return 'console unhealth'	
+						return 'unhealth'	
+			else :
+				return 'console unhealth'	
 
-	return 'health' 
+		return 'health' 
+	except Exception as e:
+		return e
 
 if __name__ == '__main__':
 	app.run(port="9000")
